878667a3884a6a35a76fb0eee8cb444512176fbc
Jonathan Cortoppassi <106403752+cortoppassi@users.noreply.github.com>
2023-08-02 21:44:55 -0300

    update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, screen} = require('electron');
 
+
 const createWindow = () => {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize; //Dimensões da área de trabalho do monitor principal.
   const windowWidth = 400; // (Número): Define a largura da janela em pixels.
@@ -18,43 +19,62 @@ const createWindow = () => {
     });
   win.loadURL('https://chat.openai.com/');
 
-  // win.webContents.openDevTools();
+  win.webContents.openDevTools();
   win.webContents.on('did-finish-load', () => { //Executar o código quando o conteúdo da janela for carregado.
+  
     win.webContents.executeJavaScript(`
         const img = document.createElement('img');
         img.src = 'https://i.pinimg.com/originals/ae/d1/1d/aed11d6975231b91c8e992c02b8376da.gif';
         img.style.position = 'absolute';
         img.style.top = '0';
         img.style.left = '0';
-        img.style.width = '440px';
-        img.style.height = '440px';
+        img.style.width = '130px';
+        img.style.height = '130px';
         img.style.zindex = '9999';
-        img.style.clipPath = 'circle(100px at center)';
+        img.style.clipPath = 'circle(30px at center)';
         img.style.objectFit = 'cover';
         img.style.overflow = 'hidden';
         document.body.appendChild(img);
 
         let divNext = document.getElementById("__next");
-        let visibilidade = true;
-        
-        const eventoDeClickImg = () => {
-            if (visibilidade) {
-                divNext.style.display = 'none';
-                minimizar();
-            } else {
-                divNext.style.display = 'block';
-            }
-            visibilidade = !visibilidade;
-            console.log('A imagem foi clicada!');
-        };
-        
-        function minimizar() {
-          console.log('Minimooo!')
-        }
+        let visibilidade = false;
 
-      img.addEventListener('click', eventoDeClickImg);
+        const eventoDeClickImg = () => {
+          if (!visibilidade) {
+            console.log('click');
+            startRecognition()
+            visibilidade = true;
+          } else {
+            console.log('clack');
+            visibilidade = false;
+          }
+        };
+
+        img.addEventListener('click', eventoDeClickImg);
+
+        function startRecognition() {
+          const recognition = new webkitSpeechRecognition() || SpeechRecognition();
+
+          recognition.lang = 'pt-BR'; // Defina o idioma do reconhecimento, neste exemplo, o Português do Brasil.
+
+          recognition.onresult = function(event) {
+              const text = event.results[0][0].transcript;
+              document.getElementById('output').innerText = "Texto reconhecido:" + text;
+          };
+
+          recognition.onerror = function(event) {
+              console.error('Erro de reconhecimento:', event.error);
+          };
+
+          recognition.onend = function() {
+              console.log('Reconhecimento encerrado.');
+          };
+
+          recognition.start();
+        }
     `);
   });
+
 };
 
 app.whenReady().then(() => {
